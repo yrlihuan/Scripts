b8bbe43d63db8912e9f1b241f0b41f2d8b186c43
Huan Li <yrlihuan@gmail.com>
2013-04-22 21:25:53 +0800

    acpi_test

--- perf_tests/test_acpi_time.py
+++ perf_tests/test_acpi_time.py
@@ -1,5 +1,19 @@
 import time
-s = 0
+from datetime import datetime
+
+
+s = ''
 for i in xrange(1000000):
-  s += time.time()
+  datetime.today().strftime("%H:%M:%S")
+
+  #t = time.time()
+  #ti = int(t)
+  #"%d:%d:%s" % (ti/3600%24, ti/60%60, t%60)
+
+
+t = time.time()
+ti = int(t)
+print "%d:%d:%s" % (ti/3600%24, ti/60%60, t%60)
+print datetime.today().strftime("%H:%M:%S")
+
 
